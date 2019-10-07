b56077dec84b69921faeff17eb2eff3a1e923c31
KLMoran85 <KLMoran85@gmail.com>
2019-10-07 10:10:20 -0400

    Add project files.

---
+++ RestaurantRater.mvc/Models/Restaurant.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.mvc.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public double Rating { get; set; }
+    }
+
+    public class RestaurantDbContext: DbContext  //making a table in our database to store this data
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }  //this class is making a connection to the database. Entity is info stored in your database.
+    }
+}
---
+++ RestaurantRater.mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRater.mvc.Startup))]
+namespace RestaurantRater.mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
