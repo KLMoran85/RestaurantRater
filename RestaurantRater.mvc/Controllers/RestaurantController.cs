e8ae664e4e21ba766f662336571ab3c4e1a3d804
KLMoran85 <KLMoran85@gmail.com>
2019-10-10 09:04:07 -0400

    updated code

---
+++ RestaurantRater.mvc/Controllers/RestaurantController.cs
@@ -0,0 +1,107 @@
+﻿using RestaurantRater.mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater.mvc.Controllers
+{
+    public class RestaurantController : Controller //<--class from our framework library  //View is another word for our html page.  Our controller is similar to our Repository and our Console.WriteLine
+    {
+        private RestaurantDbContext _db = new RestaurantDbContext();
+
+        // GET: Restaurant
+        public ActionResult Index()  //<--default home or landing page for ea section
+        {
+            return View(_db.Restaurants.ToList()); //<--we're returning to our view our db as a list   //a controller makes a connection to a "View"
+        }
+        
+        //GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Restaurant restaurant)
+        {
+            if(ModelState.IsValid)        //checking to see if everything coming in is matching what we need and is correct
+            {
+                _db.Restaurants.Add(restaurant);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+        //GET: Restaurant/Delete/{id}
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+        //POST: Restaurant/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id)
+        {
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            _db.Restaurants.Remove(restaurant);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //GET: Restaurant/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+        //POST: Restaurant/Edit {id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+
+        public ActionResult Details(int? id)  //Name my method the same as what is shows as in the index
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if(restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }  
+    }   
+}
